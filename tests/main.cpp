c2b0a38de574a185b003688be3e0ec03185cd9be
Valeriya Polikarpova <polikarp@cs.petrsu.ru>
2023-12-11 12:35:20 +0300

    fi

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,7 @@
 #include "my_sqrt_test.h"
 #include <gtest/gtest.h>
 #include "integration_test.h"
+#include "fibonachi_test.h"
 int main(int argc, char *argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
