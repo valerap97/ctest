1343177c6a624f91f65e9cd756f5a33e0af7d268
Valeriya Polikarpova <polikarp@cs.petrsu.ru>
2023-12-11 18:30:01 +0300

    Added tests

--- tests/my_sqrt_test.h
+++ tests/my_sqrt_test.h
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 #include <array>
 #include <vector>
+#include <cmath>
 
 extern "C" {
 #include "myfunc.h"
@@ -47,6 +48,30 @@ TEST(mySqrtTest, NoRealRoots) {
     ASSERT_FALSE(roots.empty());
     ASSERT_DOUBLE_EQ(roots[0], 0.0);
     ASSERT_DOUBLE_EQ(roots[1], 0.0);
+}
+
+TEST(mySqrtTest, NoARoots) {
+    double a = 0.0;
+    double b = 2.0;
+    double c = 3.0;
+
+    std::array<double, 2> roots;
+    my_sqrt(a, b, c, roots.data()); 
+    ASSERT_FALSE(roots.empty());
+    ASSERT_TRUE(std::isnan(roots[0]));
+    ASSERT_TRUE(std::isinf(roots[1]) && roots[1] < 0);
+}
+
+TEST(mySqrtTest, NoABRoots) {
+    double a = 0.0;
+    double b = 0.0;
+    double c = 3.0;
+
+    std::array<double, 2> roots;
+    my_sqrt(a, b, c, roots.data()); 
+    ASSERT_FALSE(roots.empty());
+    ASSERT_TRUE(std::isnan(roots[0]));
+    ASSERT_TRUE(std::isnan(roots[1]));
 }
 
 #endif //MYSQRT_H
